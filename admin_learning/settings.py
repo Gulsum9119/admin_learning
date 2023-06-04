2a88362fb2a6e7e11f5bb1cb3167243e1971fded
Gulsum9119 <135553856+Gulsum9119@users.noreply.github.com>
2023-06-05 02:55:17 +0500

    Update settings.py

--- admin_learning/settings.py
+++ admin_learning/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5l&mw4a3g+05@7!_veyh#%t8p(=%&ck9!*6#tsyy$-i%=*q%+5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Gulsum.pythonanywhere.com']
 
 
 # Application definition
